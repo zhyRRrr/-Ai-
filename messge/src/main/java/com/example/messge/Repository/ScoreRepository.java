df83fafdb066109b9848d0f121dc2da65421013c
zhyRRrr <167314477+zhyRRrr@users.noreply.github.com>
2024-09-30 22:15:44 +0800

    Delete messge directory

--- messge/src/main/java/com/example/messge/Repository/ScoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.messge.Repository;
-
-import com.example.messge.pojo.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-    Score findByChatId(Long chatId); // 根据聊天ID查找分数
-}
-
